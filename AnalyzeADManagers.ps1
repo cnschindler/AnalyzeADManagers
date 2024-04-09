40f055a7b35f3525cf0d87a47a387ec37b4f3786
cnschindler <cnschindler@gmx.net>
2024-04-09 18:48:00 +0200

    Updated help, minor code changes

--- AnalyzeADManagers.ps1
+++ AnalyzeADManagers.ps1
@@ -5,84 +5,58 @@
     Script to analyze the "manager" attribute on users in Active Directory. Checks if the value of the "manager"
     attribute in a certain user points to a disabled user or is empty. The scripts generates a CSV file at the end.
 .EXAMPLE
-    AnalyzeADManagers.ps1 -OutputPath C:\Reports\InvalidManagers.csv
+    AnalyzeADManagers.ps1 -OutputFolder C:\Reports
+.EXAMPLE
+    AnalyzeADManagers.ps1 -OutputFolder C:\Reports -OrganizationalUnit "Ou=Accounts,dc=domain,dc=com"
+.PARAMETER OutputFolder
+    This parameter is mandatory. The Full Path to the folder where CSV output files are stored
+.PARAMETER OrganizationalUnit
+    This parameter is optional. The distinguishedname of an organizational unit in Active Directory to begin the search
 #>
 
 [CmdLetBinding()]
 Param
 (
-    [Parameter(Mandatory=$true)]
-    $OutputPath,
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $true)]
+    $OutputFolder,
+    [Parameter(Mandatory = $false)]
     $OrganizationalUnit
 )
 
+$ScriptParentPath = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 # Region Output
-$OutputFileDisabledManagers = Join-Path -Path $OutputPath -ChildPath "DisabledManagerUsers.txt"
-$OutputFileEmptyManagers = Join-Path -Path $OutputPath -ChildPath "NoManagerSet.txt"
+$DisabledManagersFilename = 'DisabledManagerUsers.txt'
+$EmptyManagersFileName = 'NoManagerSet.txt'
+$OutputFileDisabledManagers = Join-Path -Path $OutputFolder -ChildPath $DisabledManagersFilename
+$OutputFileEmptyManagers = Join-Path -Path $OutputFolder -ChildPath $EmptyManagersFileName
 # End Region Output
 #Region Messages
-$LoadSuccess = "Successfully loaded "
-$LoadError = "Error loading "
-$LoadData = "data for "
-$LoadGroupMembers = "group members of "
-$AddSuccess = "Successfully added "
-$AddError = "Error adding "
-$ModifySuccess = "Successfully modified "
-$ModifyError = "Error modifying "
-$RemoveSuccess = "Successfully removed "
-$RemoveError = "Error removing "
-$EnableSuccess = "Successfully enabled "
-$EnableError = "Error enabling "
-$DisableSuccess = "Successfully disabled "
-$DisableError = "Error disabling "
-$InvalidOUSelection = "An invalid OU was selected. Falling back to previous value..."
-$InvalidDomainSelection = "An invalid Domain was selected. Falling back to previous value..."
-$LoadModuleSuccess = " Module successfully loaded."
-$LoadModuleError = "Error loading module "
-$ModuleNotInstalled = " Module not installed"
-$LoadSnapinSuccess = " Snapin successfully loaded."
-$LoadSnapinError = "Error loading Snapin "
-$EX_RecipientMGMTSnapin_NotInstalled = "Exchange Recipient Management Shell not installed. Please install the Exchange 2019 CU12 and above Management Tools-Only install. See: https://docs.microsoft.com/en-us/Exchange/manage-hybrid-exchange-recipients-with-management-tools"
-$RegInfo = "configuration information from registry."
-$Exists  = "exists."
-$MessageConfirmationEnabled = "Confirmation for all destructive actions is enabled." 
-$MessageConfirmationDisabled = "Confirmation for all destructive actions is disabled."
-$MessageConfirmNoConfirmation = "Are you sure you want to disable confirmation dialogs for all destructive actions?"
-$MessageLoggingEnabled = "Logging successfully enabled!"
-$MessageLoggingDisabled = "Logging successfully disabled!"
-$MessageNoDomainFound = "No Domainname could be determined. Please logon with a domain account!"
-$MessageNoAdSite = "AD Site could not be determined."
-$MessageNoDomainInfo = "No domain information could be retrieved."
-$MessageDomainFQDN = "Current Domain FQDN: "
-$MessageDomainDN = "Current Domain DN: "
-$MessageDomainCN = "Domain canonical name: "
-$MessageDCsFouned = "Domain Controllers found: "
-$MessageTargetDC = "Target Domain Controller selected: "
-$MessageDefaultOU = "Default AD container is "
-$MessageUPNSuffixes = "UPN Suffixes found:"
-$MessageNoObjectSelected = "No object was selected!"
-$MessageConfirmRemove = "Do you really want to remove "
-$MessageConfirmDisable = "Do you really want to disable "
-$MessageNOUGSelected = "Selected Group is not a Universal Group! Please select another group."
-$MessageAlreadyMailEnabled = "The selected object is already mail-enabled. Please select another object!"
-$MessageLastEmailAddress = "Cannot remove last email address!"
-$MessageMandatoryAttributesMissing = "Onject not created. Mandatory attributes missing..."
-$MessageProxyAddressNotRemoved = "Address not be removed"
-$MessageMemberNotRemoved = "Member not be removed"
-$MessageRecipientNotRemoved = "Recipient not removed"
+$LoadSuccess = 'Successfully loaded '
+$LoadError = 'Error loading '
+$LoadModuleSuccess = ' Module successfully loaded.'
+$LoadModuleError = 'Error loading module '
+$ModuleNotInstalled = ' Module not installed'
+$LoadSnapinSuccess = ' Snapin successfully loaded.'
+$LoadSnapinError = 'Error loading Snapin '
+$EX_RecipientMGMTSnapin_NotInstalled = 'Exchange Recipient Management Shell not installed. Please install the Exchange 2019 CU12 and above Management Tools-Only install. See: https://docs.microsoft.com/en-us/Exchange/manage-hybrid-exchange-recipients-with-management-tools'
+$MessageNoDomainFound = 'No Domainname could be determined. Please logon with a domain account!'
+$MessageNoDomainInfo = 'No domain information could be retrieved.'
+$MessageDomainFQDN = 'Current Domain FQDN: '
+$MessageDomainDN = 'Current Domain DN: '
+$MessageDCsFouned = 'Domain Controllers found: '
+$MessageTargetDC = 'Target Domain Controller selected: '
 #EndRegion Messages
+[scriptblock]$Userfilter = {$_.SamAccountName -ne "krbtgt" -and $_.SamAccountName -ne "Guest" -and $_.Displayname -ne "Discovery Search Mailbox" -and $_.Displayname -ne "E4E Encryption Store - Active" -and $_.Displayname -notlike "SystemMailbox*" -and $_.SamAccountName -notlike "Healthmailbox*" -and $_.Displayname -notlike "Microsoft Exchange*"}
 #Region Logging
-[string]$Script:LogFileNamePrefix = "AnalyzeADManagers"
-[string]$Script:LogfileName = ($LogFileNamePrefix + "_{0:yyyyMMdd-HHmmss}.log" -f [DateTime]::Now)
+[string]$Script:LogFileNamePrefix = 'AnalyzeADManagers'
+[string]$Script:LogfileName = ($LogFileNamePrefix + '_{0:yyyyMMdd-HHmmss}.log' -f [DateTime]::Now)
 [string]$Script:LogPath = $ScriptParentPath
 [string]$script:LogFilePath = Join-Path -Path $Script:LogPath -ChildPath $Script:LogfileName    
-[string]$Script:LogFileStart = "Logging started"
-[string]$Script:LogFileStop = "Logging stopped"
+[string]$Script:LogFileStart = 'Logging started'
+[string]$Script:LogFileStop = 'Logging stopped'
 $Script:NoLogging = $true
 #EndRegion Logging
-function Write-LogFile
-{
+function Write-LogFile {
     # Logging function, used for progress and error logging...
     # Uses the globally (script scoped) configured variables 'LogFilePath' to identify the logfile and 'NoLogging' to disable it.
     #
@@ -98,21 +72,17 @@ function Write-LogFile
     )
 
     # Prefix the string to write with the current Date and Time, add error message if present...
-    if ($ErrorInfo)
-    {
-        $logLine = "{0:d.M.y H:mm:ss} : {1}: {2} Error: {3}" -f [DateTime]::Now, $LogPrefix, $Message, $ErrorInfo.Exception.Message
+    if ($ErrorInfo) {
+        $logLine = '{0:d.M.y H:mm:ss} : {1}: {2} Error: {3}' -f [DateTime]::Now, $LogPrefix, $Message, $ErrorInfo.Exception.Message
     }
 
-    Else
-    {
-        $logLine = "{0:d.M.y H:mm:ss} : {1}: {2}" -f [DateTime]::Now, $LogPrefix, $Message
+    Else {
+        $logLine = '{0:d.M.y H:mm:ss} : {1}: {2}' -f [DateTime]::Now, $LogPrefix, $Message
     }
 
-    if (-not $NoLogging)
-    {
+    if (-not $NoLogging) {
         # Create the Script:Logfile and folder structure if it doesn't exist
-        if (-not (Test-Path $Script:LogFilePath -PathType Leaf))
-        {
+        if (-not (Test-Path $Script:LogFilePath -PathType Leaf)) {
             New-Item -ItemType File -Path $Script:LogFilePath -Force -Confirm:$false -WhatIf:$false | Out-Null
             #Add-Content -Value "Logging started." -Path $Script:LogFilePath -Encoding UTF8 -WhatIf:$false -Confirm:$false
         }
@@ -122,13 +92,11 @@ function Write-LogFile
         Write-Verbose $logLine
     }
     
-    Else
-    {
+    Else {
         Write-Host $logLine
     }
 }
-Function ManageModuleAndSnapInLoading
-{
+Function ManageModuleAndSnapInLoading {
     # Function to check for and import modules
 
     Param(
@@ -138,86 +106,72 @@ Function ManageModuleAndSnapInLoading
         [switch]$IsFile
     )
 
-    if ($Snapin)
-    {
-        if ((Get-PSSnapin $Name -Registered -ErrorAction SilentlyContinue))
-        {
-            try
-            {
+    if ($Snapin) {
+        if ((Get-PSSnapin $Name -Registered -ErrorAction SilentlyContinue)) {
+            try {
                 Add-PSSnapin -Name $Name -ErrorAction Stop
                 Write-LogFile -Message "$($Name)$($LoadSnapinSuccess)"
             }
             
-            catch
-            {
+            catch {
                 Write-LogFile -Message "$($LoadSnapinError)$($Name)." -ErrorInfo $_
             }
         }
 
-        Else
-        {
+        Else {
             Write-LogFile -Message "$($EX_RecipientMGMTSnapin_NotInstalled)"
         }
     }
 
-    Else
-    {
+    Else {
         $IsModuleInstalled = (Get-Module -ListAvailable -Name $Name | Sort-Object Version -Descending | Select-Object -First 1)
     
-        if ($IsFile)
-        {
+        if ($IsFile) {
             [System.IO.FileInfo]$Filename = $Name
-            $MatchedName = $Filename.Name.Replace(".psm1", "")
+            $MatchedName = $Filename.Name.Replace('.psm1', '')
         }
 
-        Else
-        {
+        Else {
             $MatchedName = $Name
         }
 
-        if ($IsModuleInstalled.Name -match "$($MatchedName)")
-        {   
-            try
-            {
+        if ($IsModuleInstalled.Name -match "$($MatchedName)") {   
+            try {
                 Import-Module -Name $Name -ErrorAction Stop -WarningAction SilentlyContinue -DisableNameChecking
                 Write-LogFile -Message "$($Name)$($LoadModuleSuccess)"
             }
             
-            catch
-            {
+            catch {
                 Write-LogFile -Message "$($LoadModuleError)$($Name)." -ErrorInfo $_
             }
         }
     
-        Else
-        {
+        Else {
             Write-LogFile = "$($Name)$($ModuleNotInstalled)."
             Break
         }    
     }
 }
-function Show-MessageBox
-{
+function Show-MessageBox {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string]
         $BoxTitle,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string]
         $BoxMessageText,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string]
         $BoxIcon,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [int]
         $Buttons
     )
 
     [System.Windows.Forms.MessageBox]::Show($BoxMessageText, $BoxTitle, $Buttons, $BoxIcon)
 }
-function Get-DomainControllersforDomain
-{
+function Get-DomainControllersforDomain {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -225,73 +179,65 @@ function Get-DomainControllersforDomain
         $DomainName
     )
 
-    Try
-    {
-        [array]$DCs = Get-ADDomainController -DomainName $DomainName -Discover -ErrorAction Stop| Select-Object $ColumnsDCsDatagrid
+    Try {
+        [array]$DCs = Get-ADDomainController -DomainName $DomainName -Discover -ErrorAction Stop | Select-Object $ColumnsDCsDatagrid
         Write-LogFile -Message "$($LoadSuccess) Domain Controllers."
     }
 
-    catch
-    {
+    catch {
         Write-LogFile "$($LoadError)$($ADDCs)." -ErrorInfo $_
     }
 
     Return $DCs
 }
-function Get-ManagerUser
-{
+function Get-ManagerUser {
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         $Username
     )
 
     $Filter = "Enabled -eq ""False"" -and DistinguishedName -eq ""$UserName"""
-    $Enabled = Get-ADUser -Filter $Filter -Properties DisplayName | Select-Object DisplayName,SamAccountName
+    $Enabled = Get-ADUser -Filter $Filter -Properties DisplayName | Select-Object DisplayName, SamAccountName
     Return $Enabled
 }
-function Get-ADUserManagerData
-{
+function Get-ADUserManagerData {
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory=$False)]
+        [Parameter(Mandatory = $False)]
         $OrganizationalUnit
     )
 
-    try
-    {
-        $UserManagerData = Get-ADUser -LDAPFilter '(manager=*)' -SearchBase $OrganizationalUnit -Properties Manager | Select-Object  SamAccountName,Manager
-        Write-LogFile -Message "Successfully retrieved $($UserManagerData.Count) Users from $($OrganizationalUnit)"
+    try {
+        $UserManagerData = Get-ADUser -LDAPFilter '(manager=*)' -SearchBase $OrganizationalUnit -Properties Manager | Select-Object SamAccountName, Manager
+        Write-LogFile -Message "Successfully retrieved $($UserManagerData.Count) Users with manager attribute set from $($OrganizationalUnit)"
     }
 
-    catch
-    {
+    catch {
         Write-LogFile -Message "Failure loading users from $($OrganizationalUnit)" -ErrorInfo $_
     }
 
     Return $UserManagerData
 }
-function Get-EmptyManagerAttribute
-{
+function Get-EmptyManagerAttribute {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         $OrganizationalUnit
     )
 
-    try
-    {
-        $EmptyManagerData = Get-ADUser -LDAPFilter '(!(manager=*))' -SearchBase $OrganizationalUnit -Properties Displayname | Select-Object  DisplayName,SamAccountName
-        Write-LogFile -Message "Successfully retrieved $($EmptyManagerData.Count) Users from $($OrganizationalUnit)"
+    try {
+        $EmptyManagerData = Get-ADUser -LDAPFilter '(!(manager=*))' -SearchBase $OrganizationalUnit -Properties Displayname | Select-Object DisplayName, SamAccountName
+        Write-LogFile -Message "Successfully retrieved $($EmptyManagerData.Count) Users without manager set from $($OrganizationalUnit)"
     }
 
-    catch
-    {
+    catch {
         Write-LogFile -Message "Failure loading users from $($OrganizationalUnit)" -ErrorInfo $_
     }
 
+    $EmptyManagerData = $EmptyManagerData | Where-Object -FilterScript $Userfilter
     Return $EmptyManagerData
 }
 
@@ -301,15 +247,13 @@ $Env:ADPS_LoadDefaultDrive = 0
 ManageModuleAndSnapInLoading -Name ActiveDirectory | Out-Null
 
 # Enumerate Domain Information
-if (! ($env:USERDNSDOMAIN -like "*"))
-{
+if (! ($env:USERDNSDOMAIN -like '*')) {
     Write-LogFile -Message $MessageNoDomainFound
     Show-MessageBox -BoxTitle $MSGBoxTitleNoDomainFound -BoxMessageText $MessageNoDomainFound -BoxIcon $MSGBoxStop -Buttons $MSGBoxOK
     Break
 }
 
-Else
-{
+Else {
     $Script:Domain = Get-ADDomain -Identity $env:USERDNSDOMAIN
     $Script:DomainFQDN = $Script:Domain.DNSRoot
     Write-LogFile -Message "$($MessageDomainFQDN)$($Script:DomainFQDN)."
@@ -320,51 +264,55 @@ Else
     $Script:CurrentDomainController = ($Script:DomainControllers | Select-Object -First 1).Hostname.Value
     Write-LogFile -Message "$($MessageTargetDC)$($Script:CurrentDomainController)."
     
-    if (!($Script:DomainFQDN -like "*" -and $Script:CurrentDomainController -Like "*"))
-    {
+    if (!($Script:DomainFQDN -like '*' -and $Script:CurrentDomainController -Like '*')) {
         Write-LogFile -Message $MessageNoDomainInfo
     }
 }
 
 
 # If an OU was specified
-if ($OrganizationalUnit)
-{
+if ($OrganizationalUnit) {
     # the container equals $OrganizationalUnit
     $Container = $OrganizationalUnit
 }
 
 # If no OU was specified (e.g. the whole domain should be searched)
-else
-{
+else {
     # the container is set to the domain DN
     $Container = $Script:DomainDN
 }
 
-# Query users with manager set
-$UserManagerData = Get-ADUserManagerData -OrganizationalUnit $Container
 # Query users without a manager
 $EmptyManagerData = Get-EmptyManagerAttribute -OrganizationalUnit $Container
 
-# Write CSV file with user where no manager is set
-$EmptyManagerData | Export-Csv -Path $OutputFileEmptyManagers -NoTypeInformation
+# Query users with manager set
+$UserManagerData = Get-ADUserManagerData -OrganizationalUnit $Container
 
-# Create a custom object to store disabled manager users
-$DisabledManagers = New-Object -TypeName pscustomobject
+# Create a hashtable to store disabled manager users
+$DisabledManagers = @{}
 
 # iterate through all users with manager set
-foreach ($User in $UserManagerData)
-{
+foreach ($User in $UserManagerData) {
     # Retrieve user account for manager of current loop object
     $Enabled = Get-ManagerUser -Username $user.Manager
 
     # if the manager user is disabled
-    if ($Enabled -match "\S")
-    {
-        $DisabledManagers | Add-Member -MemberType NoteProperty -Name SamAccountName -Value $Enabled.SamAccountName
-        $DisabledManagers | Add-Member -MemberType NoteProperty -Name DisplayName -Value $Enabled.DisplayName
+    if ($Enabled -match '\S') {
+        # if the user was not already added to the hashtable
+        if (-Not $DisabledManagers.ContainsKey($Enabled.SamAccountName)) {
+            $DisabledManagers.Add($Enabled.DisplayName, $enabled.SamAccountName)
+        }
     }
+
+    Clear-Variable Enabled
 }
 
-# Write CSV file with user where manager is disabled
-$DisabledManagers | Export-Csv -Path $OutputFileDisabledManagers -NoTypeInformation
+Write-LogFile -Message "Disabled manager users found: $($DisabledManagers.Count)"
+
+# Write CSV file with user where no manager is set
+Write-LogFile -Message "Writing outputfile $($OutputFileEmptyManagers)"
+$EmptyManagerData | Export-Csv -Path $OutputFileEmptyManagers -NoTypeInformation
+
+# Write CSV file with disabled manager users
+Write-LogFile -Message "Writing outputfile $($OutputFileDisabledManagers)"
+$DisabledManagers.GetEnumerator() | Select-Object @{l = 'Displayname'; e = { $_.Key } }, @{l = 'SamAccountName'; e = { $_.Value } } | Export-Csv -Path $OutputFileDisabledManagers -NoTypeInformation
